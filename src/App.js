a11df2a03e337140de23c9e931bbfa00286d5722
lamilekan263 <lamilekan263@gmail.com>
2021-01-08 09:41:30 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+
+
+import UserInfo from './component/UserInfo';
+import Repos from "./component/Repos";
+ import Activity from "./component/Activity";
+import Nav from "./component/Nav";
+
 
 function App() {
+  const [userData, setUserData] = useState(null);
+  const [repoData, setRepoData] = useState(null);
+  const [activityData, setActivityData] = useState([]);
+
+  // useEffect to search for my data
+  useEffect(() => {
+    // fetch user data
+    axios.get("https://api.github.com/users/lamilekan263").then((response) => {
+      setUserData(response.data);
+    });
+
+    // fetch repo data
+    axios
+      .get("https://api.github.com/users/lamilekan263/repos")
+      .then((response) => {
+        setRepoData(response.data);
+      });
+
+    // // // fetch activities
+    axios
+      .get("https://api.github.com/users/lamilekan263/events")
+      .then((response) => {
+        setActivityData(response.data);
+      });
+  }, []);
+console.log(repoData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      {userData && (
+        <div className="container w-full px-4 mx-auto my-5 md:my-10">
+          <div className="lg:flex lg:justify-between relative">
+            <UserInfo userData={userData} />
+            <Repos repoData={repoData} />
+            <Activity activityData={activityData} />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
